a658ce3bc558089c4111baec4991903cf68592ce
amardeep-soni <amardeepmunni@gmail.com>
2024-10-30 13:59:18 +0545

    created todo api with model serializer and model view set

--- Home/urls.py
+++ Home/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from .views import TodoViewSet
 
+router = routers.DefaultRouter()
+router.register(r"todos", TodoViewSet)
 urlpatterns = [
-    
+    path("", include(router.urls)),
 ]
